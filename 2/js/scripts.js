6b737c7fad9e594c6b589f1885ee2766308ac5c7
Steven Kutsch <skutsch40@engr-134-25.eastern.ewu.edu>
2015-02-11 13:32:48 -0800

    Part 2

--- 2/js/scripts.js
+++ 2/js/scripts.js
@@ -6,8 +6,9 @@ var prevNumMsg = 35;
 // New Number of Messages
 var newNumMsg = 123;
 // Create variable, `totalMsg` to store Total Messages
+var totalMsg = prevNumMsg + newNumMsg;
 // Add (sum) of Previous and New Messages
-var totalMsg;
-
+console.log(totalMsg);
 // Output to red Messages badge
+$('.red').text(totalMsg);
 // REF: http://api.jquery.com/text/
